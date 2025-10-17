38f05aafec28869978710166b380cfbf54c60d6c
Evelin Cobos <evecobos8@gmail.com>
2025-10-16 23:02:10 -0400

    complete

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,32 +1,82 @@
-const coolGreeting = () => {
+const coolGreeting = (person) => {
+  if (person.isCool === true) {
+    return `What is UP ${person.name.toUpperCase()}? How you been doin'?`
+  } else {
+    return `Greetings ${person.name}, how have you been lately?`
+  }
 };
 
-const haveBirthday = () => {
+const haveBirthday = (person) => {
+  person.age += 1
 };
 
-const becomeSecretAgent = () => {
+const becomeSecretAgent = (person, spyHandle) => {
+  person.spyHandle = spyHandle
+  delete person.name
 };
 
-const carMaker = () => {
+const carMaker = (name, maker, year) => {
+  let obj = {
+    name,
+    maker,
+    year,
+    needsOilChange: false
+  }
+  return obj
 };
 
-const weAreNotFriends = () => {
+const weAreNotFriends = (person) => {
+  return person.friends.pop()
 };
 
-const listHobbies = () => {
+const listHobbies = (person) => {
+  for (let hobby of person.hobbies) {
+    console.log(`${person.name} likes ${hobby}.`)
+  }
 };
 
-const getNextOpponent = () => {
+const getNextOpponent = (team) => {
+  if (team.matches.length === 0) {
+    return null
+  }
+  return team.matches[0].teamName
 };
 
-const listAllKeys = () => {
+const listAllKeys = (object) => {
+  let arr = Object.keys(object)
+  return arr
+
+  // let arr = []
+  // for (let key in object) {
+  //   arr.push(key)
+  // }
+  // return arr
 };
 
-const listAllValues = () => {
+const listAllValues = (object) => {
+  let arr = Object.values(object)
+  return arr
 };
 
-const convertToMatrix = () => {
+const convertToMatrix = (arrOfObj) => {
+  if (arrOfObj.length === 0) {
+    return []
+  }
+
+  let newArr = []
+  let arrOfKeys = Object.keys(arrOfObj[0])
+  newArr.push(arrOfKeys)
+
+  for (i = 0; i < arrOfObj.length; i++) {
+    let arrOfValues = Object.values(arrOfObj[i])
+    newArr.push(arrOfValues)
+  }
+
+  return newArr
+
 };
+
+
 
 module.exports = {
   coolGreeting,
